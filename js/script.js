13f6c0c7dad5b22dfb098198cd4e80a8883c4ac9
aromot <aromot@gmail.com>
2021-01-23 16:23:23 +0100

    Changement d'API weather

--- js/script.js
+++ js/script.js
@@ -7,7 +7,10 @@ $("#select-city").change(
             return;
         }
 
-        var url = "https://www.metaweather.com/api/location/" + id;
+        var key = '779b060de8050ff37c4b63a8310bc9c4';
+
+        // var url = "https://www.metaweather.com/api/location/" + id;  // fonctionne pas
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=Tangier&appid=" + key;
 
         $.getJSON(url, json => {
             console.log(json);
