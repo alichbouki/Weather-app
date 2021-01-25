6d643c62f7f3f93b0cfbfbed7f882027f9594b05
aromot <aromot@gmail.com>
2021-01-25 18:43:41 +0100

    Ajout de l'asynchrone

--- js/script.js
+++ js/script.js
@@ -1,19 +1,58 @@
-$("#select-city").change(
-    evt => {
-        var id = evt.target.value;
-        console.log(id);
-        
-        if (id === ""){
-            return;
-        }
 
-        var key = '779b060de8050ff37c4b63a8310bc9c4';
+
+$("#select-city").change(
+    async evt => {
+        var id = evt.target.value;
+        
+        if (id === "")
+            return;
 
         // var url = "https://www.metaweather.com/api/location/" + id;  // fonctionne pas
-        var url = "https://api.openweathermap.org/data/2.5/weather?q=Tangier&appid=" + key;
+        var key = '779b060de8050ff37c4b63a8310bc9c4';
+        var url = "https://api.openweatgrthrthhermap.org/data/2.5/weather?q="+id+"&appid=" + key;
 
-        $.getJSON(url, json => {
-            console.log(json);
-        });
+        // Method 1
+        // $.getJSON(url, success);
+        // console.log("Ici après le $.getJSON");
+
+        // Method 2
+        // $.getJSON(url).then(success);
+        // var result = $.getJSON(url);
+        // console.log('result:', result);
+        // result.done(success);
+        // result.fail(error);
+
+        // Method 3 (async)
+        try {
+            console.log("1. Before");
+            var json = await getWeather(id);
+            console.log("4. after getWeather");
+            success(json);
+            console.log("5. THE END");
+        } catch (error) {
+            console.log("Error", error);            
+        }
     }
 );
+
+function getWeather(id) {
+    console.log("2. start getWeather");
+
+    var key = '779b060de8050ff37c4b63a8310bc9c4';
+    var url = "https://api.openweathermap.org/data/2.5/weather?q="+id+"&appid=" + key;
+    var myPromise = $.getJSON(url);
+    console.log("3. end getWeather");
+    return myPromise;
+}
+
+function success(data) {
+    console.log(">>> Dans la callback Success");
+    // console.log(data);
+    $('code').text(JSON.stringify(data, null, 2));    
+}
+
+function error(err) {
+    console.log("Il y a une erreur");
+    console.log(err);
+    $('code').text("ERROR !!!");    
+}
